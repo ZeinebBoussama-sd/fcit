e7fff77303da6d3928b8fcd634917afa4533ce51
ZeinebBoussama-sd <boussamazeineb98@gmail.com>
2020-03-24 16:04:03 +0100

    start

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  return (
+    <div>
+      <button type="button" class="btn btn-primary">
+        Primary
+      </button>
+    </div>
+  );
+}
+
+export default App;

c4ada0940e1a3a3515963e4c415e25245efbe14f
ZeinebBoussama-sd <boussamazeineb98@gmail.com>
2020-03-26 18:13:34 +0100

    nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Nav from "./component/Nav";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./foundation/style.css";
 function App() {
   return (
     <div>
-      <button type="button" class="btn btn-primary">
+      <Nav />
+      <button type="button" class="btn btn-success">
         Primary
       </button>
     </div>

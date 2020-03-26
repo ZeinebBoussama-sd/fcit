02fdbc7e8491b965ce77ec077a99695f8f62a6b6
sadzai <sadok.zaibi@gmail.com>
2020-03-27 00:10:35 +0100

    dropdown fix

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -31,6 +31,89 @@ function Nav() {
             <li class='nav-item dropdown'>
               <a
                 class='nav-link dropdown-toggle'
+                href='http://example.com'
+                id='navbarDropdownMenuLink'
+                data-toggle='dropdown'
+                aria-haspopup='true'
+                aria-expanded='false'
+              >
+                {' '}
+                Dropdown{' '}
+              </a>
+              <ul
+                class='dropdown-menu'
+                aria-labelledby='navbarDropdownMenuLink'
+              >
+                <li>
+                  <a class='dropdown-item' href='#'>
+                    Action
+                  </a>
+                </li>
+                <li>
+                  <a class='dropdown-item' href='#'>
+                    Another action
+                  </a>
+                </li>
+                <li class='dropdown-submenu'>
+                  <a
+                    class='dropdown-item dropdown-toggle'
+                    href='http://google.com'
+                  >
+                    Google
+                  </a>
+                  <ul class='dropdown-menu'>
+                    <li>
+                      <a class='dropdown-item' href='#'>
+                        Submenu
+                      </a>
+                    </li>
+                    <li>
+                      <a class='dropdown-item' href='#'>
+                        Submenu0
+                      </a>
+                    </li>
+                    <li class='dropdown-submenu'>
+                      <a class='dropdown-item dropdown-toggle' href='#'>
+                        Submenu 1
+                      </a>
+                      <ul class='dropdown-menu'>
+                        <li>
+                          <a class='dropdown-item' href='#'>
+                            Subsubmenu1
+                          </a>
+                        </li>
+                        <li>
+                          <a class='dropdown-item' href='#'>
+                            Subsubmenu1
+                          </a>
+                        </li>
+                      </ul>
+                    </li>
+                    <li class='dropdown-submenu'>
+                      <a class='dropdown-item dropdown-toggle' href='#'>
+                        Submenu 2
+                      </a>
+                      <ul class='dropdown-menu'>
+                        <li>
+                          <a class='dropdown-item' href='#'>
+                            Subsubmenu2
+                          </a>
+                        </li>
+                        <li>
+                          <a class='dropdown-item' href='#'>
+                            Subsubmenu2
+                          </a>
+                        </li>
+                      </ul>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+
+            <li class='nav-item dropdown'>
+              <a
+                class='nav-link dropdown-toggle'
                 href='#'
                 id='navbarDropdown'
                 role='button'
@@ -41,33 +124,9 @@ function Nav() {
                 Nos Solutions
               </a>
               <div class='dropdown-menu' aria-labelledby='navbarDropdown'>
-                <li class='nav-item dropright'>
-                  <a
-                    class='dropdown-item nav-link dropdown-toggle'
-                    href='#'
-                    id='navbarDropdown'
-                    role='button'
-                    data-toggle='dropdown'
-                    aria-haspopup='true'
-                    aria-expanded='false'
-                  >
-                    Nos formations
-                  </a>
-                  <div class='dropdown-menu' aria-labelledby='navbarDropdown'>
-                    <a class='dropdown-item' href='#'>
-                      formation
-                    </a>
-                    <a class='dropdown-item' href='#'>
-                      cursus
-                    </a>
-                    <div class='dropdown-divider'></div>
-                    <a class='dropdown-item' href='#'>
-                      Something else here
-                    </a>
-                  </div>
-                </li>
-
-                <div class='dropdown-divider'></div>
+                <a class='dropdown-item' href='#'>
+                  Formation
+                </a>
                 <a class='dropdown-item' href='#'>
                   Séminaire
                 </a>

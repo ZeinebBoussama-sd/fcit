4680b8857b0af9834d59793414f704f9d58593ba
ZeinebBoussama-sd <boussamazeineb98@gmail.com>
2020-03-28 01:17:52 +0100

    footer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import Nav from './component/Nav';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './foundation/style.css';
+import React from "react";
+import Nav from "./component/Nav";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./foundation/style.css";
+import Footer from "./component/Footer";
+import "@fortawesome/fontawesome-free";
 function App() {
   return (
     <div>
       <Nav />
-      <button type='button' class='btn btn-success'>
+      <button type="button" class="btn btn-success">
         Primary
       </button>
+      <Footer />
     </div>
   );
 }
---
+++ src/component/Footer.js
@@ -0,0 +1,37 @@
+import React from "react";
+import logofcit from "../foundation/logo/logofcit3.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {fa-mobile-alt' } from "@fortawesome/free-solid-svg-icons";
+function Footer() {
+  return (
+    <div>
+      <div class="container-fluid bg-dark text-white">
+        <div class="row">
+          <div class="col-lg-1">
+            <a href="#">
+              <img src={logofcit} height="100" width="100" alt=""></img>
+            </a>
+          </div>
+          <div class="col-lg-3 text-center font-italic">
+            FC-IT est un cabinet de formation et de conseil en nouvelles
+            technologies de l'information.
+          </div>
+          <div class="col-lg-2 text-center font-italic">
+            contact@fc4it.net espace.clients@fc4it.net
+          </div>
+          <div class="col-lg-3 text-center font-italic">
+            <FontAwesomeIcon icon="fa-mobile-alt" color="blue" />
+            Tél: +21656563753 Office:+21656563643/+21656563633/ +21656563833
+          </div>
+          <div class="col-lg-3 text-center font-italic">
+            Centre Millenium 2éme étage-Bureau N°19 Route de la Marsa, Sidi
+            Daoud La Marsa, Tunisie Mindup, Bureau 1, 8 avenue de l’université
+            Manar1 , Tunis, Tunisie
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;

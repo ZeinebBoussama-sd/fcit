38a98f6bc00f875c0619970e3e3ce66f8b454b55
ZeinebBoussama-sd <boussamazeineb98@gmail.com>
2020-03-28 02:49:03 +0100

    <p>

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -15,22 +15,46 @@ function Footer() {
               <img src={logofcit} height="100" width="100" alt=""></img>
             </a>
           </div>
-          <div class="col-lg-3 text-center font-italic">
+          <div class="col-lg-3 text-left font-italic">
             FC-IT est un cabinet de formation et de conseil en nouvelles
             technologies de l'information.
           </div>
-          <div class="col-lg-2 text-center font-italic">
-            <FontAwesomeIcon icon={faEnvelopeOpenText} color="blue" />
-            contact@fc4it.net espace.clients@fc4it.net
+          <div class="col-lg-2 text-left font-italic">
+            <p>
+              <FontAwesomeIcon
+                icon={faEnvelopeOpenText}
+                color="cornflowerblue	"
+              />
+              contact@fc4it.net{" "}
+            </p>
+
+            <p>
+              <FontAwesomeIcon
+                icon={faEnvelopeOpenText}
+                color="cornflowerblue	"
+              />
+              espace.clients@fc4it.net
+            </p>
           </div>
-          <div class="col-lg-3 text-center font-italic">
-            <FontAwesomeIcon icon={faMobileAlt} color="blue" />
-            Tél: +21656563753 Office:+21656563643/+21656563633/ +21656563833
+          <div class="col-lg-3 text-left font-italic">
+            <p>
+              <FontAwesomeIcon icon={faMobileAlt} color="cornflowerblue	" />
+              Tél: +21656563753{" "}
+            </p>
+
+            <p>
+              <FontAwesomeIcon icon={faMobileAlt} color="cornflowerblue	" />
+              Office:+21656563643/+21656563633/ +21656563833{" "}
+            </p>
           </div>
-          <div class="col-lg-3 text-center font-italic">
-            Centre Millenium 2éme étage-Bureau N°19 Route de la Marsa, Sidi
-            Daoud La Marsa, Tunisie Mindup, Bureau 1, 8 avenue de l’université
-            Manar1 , Tunis, Tunisie
+          <div class="col-lg-3 text-left font-italic">
+            <p>
+              Centre Millenium 2éme étage-Bureau N°19 Route de la Marsa, Sidi
+              Daoud La Marsa
+            </p>
+            <p>
+              Mindup, Bureau 1, 8 avenue de l’université Manar1 , Tunis, Tunisie
+            </p>
           </div>
         </div>
       </div>

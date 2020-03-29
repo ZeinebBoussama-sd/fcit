79bcd8c580eb0ccea5c3ce4d7eb90ddaddaf3abb
sadzai <sadok.zaibi@gmail.com>
2020-03-30 00:12:09 +0200

    fix bg image in pt1

--- src/component/Home/Pt1.js
+++ src/component/Home/Pt1.js
@@ -1,22 +1,15 @@
-import React from "react";
-import formation from "../../foundation/home/methodologie-formation.jpg";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function Pt1() {
   return (
-    <div>
-      <div style={{ position: "relative", height: "400px" }}>
-        <div style={{ position: "absolute", zIndex: "1" }}>
-          <img src={formation} class="img-gd" alt="Responsive image" />
-        </div>
-        <div class="div-img" style={{ zIndex: "2", fontSize: "200%" }}>
-          <h5 class="align-top" style={{ color: "white", margin: "20px" }}>
-            Formation & consulting information technology
-          </h5>
-          <h1 class="h1-img1 align-top">
-            Bienvenue chez le spécialiste de la formation contenue et du
-            consulting
-          </h1>
-        </div>
+    <div class='pt1'>
+      <div class='pt1_text'>
+        <p>Formation & consulting information technology</p>
+        <h1>
+          Bienvenue chez le spécialiste de la formation contenue et du
+          consulting
+        </h1>
       </div>
     </div>
   );

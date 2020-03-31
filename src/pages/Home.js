79ef50729a12329701daf02257e9d347419d4a7c
ZeinebBoussama-sd <boussamazeineb98@gmail.com>
2020-03-31 16:28:23 +0200

    acceuil done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,12 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Pt1 from "../component/Home/Pt1";
 import Pt2 from "../component/Home/Pt2";
+import Pt3 from "../component/Home/Pt3";
 function Home() {
   return (
     <div className="Home">
       <Pt1 />
+      <Pt3 />
       <Pt2 />
     </div>
   );

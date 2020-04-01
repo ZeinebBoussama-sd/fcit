f56ddd4edfa637571fcdf3d533660d3552f0e2e4
ZeinebBoussama-sd <boussamazeineb98@gmail.com>
2020-04-01 20:56:28 +0200

    propos

---
+++ src/component/Apropos/Apropos.js
@@ -0,0 +1,16 @@
+import React from "react";
+function Apropos() {
+  return (
+    <div class name="Apropos">
+      <p>FCIT: QUI SOMMES-NOUS ?</p>
+      <h3>Le spécialiste de la formation continue</h3>
+      <p>
+        FCIT est un organisme de formations informatiques à Tunis destiné aux
+        professionnels de l’informatique. La société est constituée d’ingénieurs
+        informaticiens spécialistes de leur domaine, et qui disposent d'une
+        expérience d'au moins 10 ans dans leurs activités d'expert(s).
+      </p>
+    </div>
+  );
+}
+export default Apropos;
---
+++ src/pages/Apropos.js
@@ -0,0 +1,10 @@
+import React from "react";
+function APropos() {
+  return (
+    <div class name="Apropos">
+      <Aprops />
+      <Pt3 />
+    </div>
+  );
+}
+export default Apropos;

ef61e85a06e261c611cefad3262b0bfd7e4d71fe
sadzai <sadok.zaibi@gmail.com>
2020-04-13 20:45:36 +0200

    fix style

--- client/src/component/Home/Pt1.js
+++ client/src/component/Home/Pt1.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 function Pt1() {
   return (
-    <div className='pt1 text-white'>
-      <div className='pt1_text'>
-        <h2>Formation & consulting information technology</h2>
-        <h4>
-          Bienvenue chez le spécialiste de la formation contenue et du
-          consulting
-        </h4>
+    <div className='pt1 height-9 text-white'>
+      <div className='height-9 d-flex align-items-center align-middle'>
+        <div className='col-md-6 offset-md-5'>
+          <h2>Formation & consulting information technology</h2>
+          <h4>
+            Bienvenue chez le spécialiste de la formation contenue et du
+            consulting
+          </h4>
+        </div>
       </div>
     </div>
   );

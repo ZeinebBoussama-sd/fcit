71b9202dcb6e8d24a3a740d2dff326599b141c0a
ZeinebBoussama-sd <boussamazeineb98@gmail.com>
2020-04-13 21:03:55 +0200

    aaa

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
-const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServer, gql } = require("apollo-server");
 
-const 
 const typeDefs = gql`
- 
-  type Client{
+  type Client {
     idClient: ID!
     nomClient: String
-    emailClient:  String!
-    telClient:Int
+    emailClient: String!
+    telClient: Int
     AdrClient: String
   }
 
@@ -15,97 +13,112 @@ const typeDefs = gql`
     cinP: Int!
     client: Client
   }
-  type Societé {
-    MatfiscaleSocieté: Int!
+  type Societe {
+    MatfiscaleSociete: Int!
     client: Client
   }
 
   type DemandeFormation {
-      idFormation:ID!
-      dateDemande: Date
-      datebecprev: Date 
-      typeDemande: String
-      EtatDemande: String
-      honorairePrevu : Int
-      lieuPrevu: String
-      dureePrevu: Int
-      horairePrevu: String
-      client: Client
-      formation :Formation
+    idFormation: ID!
+    dateDemande: Date
+    datebecprev: Date
+    typeDemande: String
+    EtatDemande: String
+    honorairePrevu: Int
+    lieuPrevu: String
+    dureePrevu: Int
+    horairePrevu: String
+    client: Client
+    formation: Formation
   }
-  type Session{
-      idSession : ID!
-      typeSess: String
-      dateDebSess: Date
-      dureeSess : Int
-      horaireSess : Double
-      fraisSejour: Float
-      fraisTransport: Float
-      fraisAutres: Float 
-      client: Client
-      support: Support
-      formateur: Formateur 
-      participant:Participant
-      formation:Formation
+  type Session {
+    idSession: ID!
+    typeSess: String
+    dateDebSess: Date
+    dureeSess: Int
+    horaireSess: Double
+    fraisSejour: Float
+    fraisTransport: Float
+    fraisAutres: Float
+    client: Client
+    support: Support
+    formateur: Formateur
+    participant: Participant
+    formation: Formation
   }
-type Formation{
+  type Formation {
     refFormation: String
     intituleFormation: String
-    duréeFormation: Int
+    dureeFormation: Int
     honoraireFormation: Int
     nbreMinPart: Int
     nbreMaxPart: Int
     theme: Theme
-    motclé: MotClé
+    motcle: MotCle
     formateur: Formateur
     demandeformation: DemandeFormation
-}
-type Formateur{
-    idFormateur : ID!
-    nomF : String
-    prenomF : String
+  }
+  type Formateur {
+    idFormateur: ID!
+    nomF: String
+    prenomF: String
     cvF: String
-    email :String
+    email: String
     telF: Int
-    NSSF :Int
+    NSSF: Int
     salaire: Float
-    specialitéF: String
+    specialiteF: String
     AdrF: String
     dateAjout: Date
     formation: Formation
     session: [Session]
-    motclé: [MotClé]
-    support: [Support] 
-}
-type  MotClé {
-    idMotClé: ID!
+    motcle: [MotCle]
+    support: [Support]
+  }
+  type MotCle {
+    idMotCle: ID!
     formation: Formation
     formateur: Formateur
-}
-type Support {
+  }
+  type Support {
     idSupport: ID!
-    nomSupport : String
+    nomSupport: String
     typeSupport: String
     formateur: Formateur
     ingenieurpedagogique: IngenieurPedagogique
     fichier: [Fichier]
-}
-type Fichier{
+  }
+  type Fichier {
     idFichier: ID!
     nomfichier: String
     typefichier: String
     taillemax: Int
     urlfichier: String
-}
-
-type Query {
-  books: [Book]
-  authors: [Author]
-}`;
+  }
+  type theme {
+    idTheme: ID!
+    nom_theme: String
+  }
+  type participant {
+    idParticipant: ID!
+    CIN: Int
+    nom_partcipant: String
+    prenom_participant: String
+  }
+  type IngenieurPedagogique {
+    id_ingenieurPedagogique: ID!
+    nom_ing: String
+    prenom_ing: String
+  }
+  type Query {
+    books: [Book]
+    authors: [Author]
+  }
+`;
 const resolvers = {
   Query: {
-    books: () => books
-  }
+    books: () => books,
+  },
 };
 const server = new ApolloServer({ typeDefs, resolvers });
 

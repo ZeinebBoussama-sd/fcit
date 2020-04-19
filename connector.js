258d0f43e2da5ebf9052ca0d22f560506b58529a
ZeinebBoussama-sd <boussamazeineb98@gmail.com>
2020-04-20 00:53:53 +0200

    sequelize

---
+++ connector.js
@@ -0,0 +1,4 @@
+const db = new Sequelize("fcit", null, null, {
+  dialect: "sqlite",
+  storage: "./fcit.sqlite",
+});
--- server.js
+++ server.js
@@ -1,127 +1,42 @@
-const { ApolloServer, gql } = require("apollo-server");
+const server = new ApolloServer({ typeDefs });
+const { gql } = require("apollo-server");
 
 const typeDefs = gql`
-  type Client {
-    idClient: ID!
-    nomClient: String
-    emailClient: String!
-    telClient: Int
-    AdrClient: String
+  type Launch {
+    id: ID!
+    site: String
+    mission: Mission
+    rocket: Rocket
+    isBooked: Boolean!
+  }
+  type Rocket {
+    id: ID!
+    name: String
+    type: String
   }
 
-  type Personne {
-    cinP: Int!
-    client: Client
-  }
-  type Societe {
-    MatfiscaleSociete: Int!
-    client: Client
+  type User {
+    id: ID!
+    email: String!
+    trips: [Launch]!
   }
 
-  type DemandeFormation {
-    idFormation: ID!
-    dateDemande: Date
-    datebecprev: Date
-    typeDemande: String
-    EtatDemande: String
-    honorairePrevu: Int
-    lieuPrevu: String
-    dureePrevu: Int
-    horairePrevu: String
-    client: Client
-    formation: Formation
+  type Mission {
+    name: String
+    missionPatch(size: PatchSize): String
   }
-  type Session {
-    idSession: ID!
-    typeSess: String
-    dateDebSess: Date
-    dureeSess: Int
-    horaireSess: Double
-    fraisSejour: Float
-    fraisTransport: Float
-    fraisAutres: Float
-    client: Client
-    support: Support
-    formateur: Formateur
-    participant: Participant
-    formation: Formation
-  }
-  type Formation {
-    refFormation: String
-    intituleFormation: String
-    dureeFormation: Int
-    honoraireFormation: Int
-    nbreMinPart: Int
-    nbreMaxPart: Int
-    theme: Theme
-    motcle: MotCle
-    formateur: Formateur
-    demandeformation: DemandeFormation
-  }
-  type Formateur {
-    idFormateur: ID!
-    nomF: String
-    prenomF: String
-    cvF: String
-    email: String
-    telF: Int
-    NSSF: Int
-    salaire: Float
-    specialiteF: String
-    AdrF: String
-    dateAjout: Date
-    formation: Formation
-    session: [Session]
-    motcle: [MotCle]
-    support: [Support]
-  }
-  type MotCle {
-    idMotCle: ID!
-    formation: Formation
-    formateur: Formateur
-  }
-  type Support {
-    idSupport: ID!
-    nomSupport: String
-    typeSupport: String
-    formateur: Formateur
-    ingenieurpedagogique: IngenieurPedagogique
-    fichier: [Fichier]
-  }
-  type Fichier {
-    idFichier: ID!
-    nomfichier: String
-    typefichier: String
-    taillemax: Int
-    urlfichier: String
-  }
-  type theme {
-    idTheme: ID!
-    nom_theme: String
-  }
-  type participant {
-    idParticipant: ID!
-    CIN: Int
-    nom_partcipant: String
-    prenom_participant: String
-  }
-  type IngenieurPedagogique {
-    id_ingenieurPedagogique: ID!
-    nom_ing: String
-    prenom_ing: String
+
+  enum PatchSize {
+    SMALL
+    LARGE
   }
   type Query {
-    books: [Book]
-    authors: [Author]
+    launches: [Launch]!
+    launch(id: ID!): Launch
+    me: User
   }
 `;
-const resolvers = {
-  Query: {
-    books: () => books,
-  },
-};
-const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
+  console.log(`🚀 Server ready at ${url}`);
 });

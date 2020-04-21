c35178f1f394cfe99c47be0ccc75ac1f07fb6645
ZeinebBoussama-sd <boussamazeineb98@gmail.com>
2020-04-21 21:36:29 +0200

    ss

--- server.js
+++ server.js
@@ -1,42 +1,18 @@
-const server = new ApolloServer({ typeDefs });
-const { gql } = require("apollo-server");
+const { ApolloServer } = require("apollo-server");
 
-const typeDefs = gql`
-  type Launch {
-    id: ID!
-    site: String
-    mission: Mission
-    rocket: Rocket
-    isBooked: Boolean!
-  }
-  type Rocket {
-    id: ID!
-    name: String
-    type: String
-  }
+const db = require("./connector");
 
-  type User {
-    id: ID!
-    email: String!
-    trips: [Launch]!
-  }
+//test connection
+db.authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
 
-  type Mission {
-    name: String
-    missionPatch(size: PatchSize): String
-  }
-
-  enum PatchSize {
-    SMALL
-    LARGE
-  }
-  type Query {
-    launches: [Launch]!
-    launch(id: ID!): Launch
-    me: User
-  }
-`;
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
+  console.log(`🚀  Server ready at ${url}`);
 });

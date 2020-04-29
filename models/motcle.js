7dc6f04c43f48aa9b1762b93a0056a26a68b9d11
ZeinebBoussama-sd <boussamazeineb98@gmail.com>
2020-04-29 19:19:56 +0200

    0.1

--- models/motcle.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-module.exports = (sequelize, DataTypes) => {
-  const MotCle = sequelize.define("MotCle", {}, {});
-  MotCle.associate = function (models) {
-    // associations can be defined here
-  };
-  return MotCle;
-};

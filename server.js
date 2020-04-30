6b8320c0673da9f54ddd651792619e086a677cff
sadzai <sadok.zaibi@gmail.com>
2020-04-30 18:46:05 +0200

    origin

--- server.js
+++ server.js
@@ -1,18 +1,112 @@
-const { ApolloServer } = require('apollo-server');
-const typeDefs = require('./schema');
-const db = require('./src/connector');
-const resolvers = require('./src/resolvers');
-const user = require('./src/modals/Users');
-//test connection
-db.authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err);
-  });
-user;
-db.sync();
+const { ApolloServer, gql } = require('apollo-server');
+
+const 
+const typeDefs = gql`
+ 
+  type Client{
+    idClient: ID!
+    nomClient: String
+    emailClient:  String!
+    telClient:Int
+    AdrClient: String
+  }
+
+  type Personne {
+    cinP: Int!
+    client: Client
+  }
+  type Societé {
+    MatfiscaleSocieté: Int!
+    client: Client
+  }
+
+  type DemandeFormation {
+      idFormation:ID!
+      dateDemande: Date
+      datebecprev: Date 
+      typeDemande: String
+      EtatDemande: String
+      honorairePrevu : Int
+      lieuPrevu: String
+      dureePrevu: Int
+      horairePrevu: String
+      client: Client
+      formation :Formation
+  }
+  type Session{
+      idSession : ID!
+      typeSess: String
+      dateDebSess: Date
+      dureeSess : Int
+      horaireSess : Double
+      fraisSejour: Float
+      fraisTransport: Float
+      fraisAutres: Float 
+      client: Client
+      support: Support
+      formateur: Formateur 
+      participant:Participant
+      formation:Formation
+  }
+type Formation{
+    refFormation: String
+    intituleFormation: String
+    duréeFormation: Int
+    honoraireFormation: Int
+    nbreMinPart: Int
+    nbreMaxPart: Int
+    theme: Theme
+    motclé: MotClé
+    formateur: Formateur
+    demandeformation: DemandeFormation
+}
+type Formateur{
+    idFormateur : ID!
+    nomF : String
+    prenomF : String
+    cvF: String
+    email :String
+    telF: Int
+    NSSF :Int
+    salaire: Float
+    specialitéF: String
+    AdrF: String
+    dateAjout: Date
+    formation: Formation
+    session: [Session]
+    motclé: [MotClé]
+    support: [Support] 
+}
+type  MotClé {
+    idMotClé: ID!
+    formation: Formation
+    formateur: Formateur
+}
+type Support {
+    idSupport: ID!
+    nomSupport : String
+    typeSupport: String
+    formateur: Formateur
+    ingenieurpedagogique: IngenieurPedagogique
+    fichier: [Fichier]
+}
+type Fichier{
+    idFichier: ID!
+    nomfichier: String
+    typefichier: String
+    taillemax: Int
+    urlfichier: String
+}
+
+type Query {
+  books: [Book]
+  authors: [Author]
+}`;
+const resolvers = {
+  Query: {
+    books: () => books
+  }
+};
 const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {

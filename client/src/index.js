235dbfa620bb92ae9e9c15f9aa3e714fc21185b7
ZeinebBoussama-sd <boussamazeineb98@gmail.com>
2020-05-11 18:11:06 +0200

    client0

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import ApolloClient from "apollo-boost";
 
+const client = new ApolloClient({
+  uri: "https://48p1r2roz4.sse.codesandbox.io",
+});
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change

fdf6918325bde001044c7f2047e2df2c0200c404
sadzai <sadok.zaibi@gmail.com>
2020-05-11 18:21:16 +0200

    small fix

--- client/src/component/Home/Pt4.js
+++ client/src/component/Home/Pt4.js
@@ -5,30 +5,28 @@ import ib from '../../foundation/home/ib.png';
 
 function Pt4() {
   return (
-    <div>
-      <div className='container'>
-        <div className='row  pt-5 pb-5'>
-          <div className='col-md-4 pt-4 pb-3'>
-            <center>
-              <a href='https://www.orsys.fr/' target='_blanc'>
-                <img src={orsys} alt='' />
-              </a>
-            </center>
-          </div>
-          <div className='col-md-4 pt-4 pb-3 drop-shadow'>
-            <center>
-              <a href='https://www.plb.fr/' target='_blanc'>
-                <img src={PLB} alt='' />
-              </a>
-            </center>
-          </div>
-          <div className='col-md-4 pt-4 pb-3'>
-            <center>
-              <a href='https://www.ib-formation.fr/' target='_blanc'>
-                <img src={ib} alt='' />
-              </a>
-            </center>
-          </div>
+    <div className='container'>
+      <div className='row  pt-5 pb-5'>
+        <div className='col-md-4 pt-4 pb-3'>
+          <center>
+            <a href='https://www.orsys.fr/' target='_blanc'>
+              <img src={orsys} alt='' />
+            </a>
+          </center>
+        </div>
+        <div className='col-md-4 pt-4 pb-3 drop-shadow'>
+          <center>
+            <a href='https://www.plb.fr/' target='_blanc'>
+              <img src={PLB} alt='' />
+            </a>
+          </center>
+        </div>
+        <div className='col-md-4 pt-4 pb-3'>
+          <center>
+            <a href='https://www.ib-formation.fr/' target='_blanc'>
+              <img src={ib} alt='' />
+            </a>
+          </center>
         </div>
       </div>
     </div>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,11 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Pt1 from "../component/Home/Pt1";
-import Pt2 from "../component/Home/Pt2";
-import Pt3 from "../component/Home/Pt3";
-import Pt4 from "../component/Home/Pt4";
+import React from 'react';
+import Pt1 from '../component/Home/Pt1';
+import Pt2 from '../component/Home/Pt2';
+import Pt3 from '../component/Home/Pt3';
+import Pt4 from '../component/Home/Pt4';
 function Home() {
   return (
-    <div className="Home">
+    <div>
       <Pt1 />
       <Pt3 />
       <Pt2 />

6fd8b73a52cba56a6229bbb64cf8431fef288ef7
sadzai <sadok.zaibi@gmail.com>
2020-05-11 19:29:12 +0200

    fix admin

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,6 +1,26 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
 
 function Admin() {
-  return <div>test</div>;
+  const { loading, error, data } = useQuery(gql`
+    {
+      allClient {
+        id
+        nom_client
+      }
+    }
+  `);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return (
+    <div className=' mt-8 '>
+      {data.allClient.map(({ id, nom_Client }) => (
+        <p key={id}>{nom_Client}</p>
+      ))}
+    </div>
+  );
 }
 export default Admin;

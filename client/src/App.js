63c6d1dac4a2f17b9aa4135464dbc07f38d6bf77
sadzai <sadok.zaibi@gmail.com>
2020-05-11 18:22:38 +0200

    add admin page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Nav from './component/Nav';
 import Home from './pages/Home';
+import Admin from './pages/Admin';
 import Footer from './component/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './foundation/style.css';
@@ -16,6 +17,7 @@ function App() {
         <Switch>
           <Route path='/' exact component={Home} />
           <Route path='/apropos' exact component={Apropos} />
+          <Route path='/admin' exact component={Admin} />
         </Switch>
         <Footer />
       </BrowserRouter>

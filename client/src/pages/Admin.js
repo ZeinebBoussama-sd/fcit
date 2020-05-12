0df8752b6a2631f9246fa7c3c78b57011cd6dad5
ZeinebBoussama-sd <boussamazeineb98@gmail.com>
2020-05-12 19:21:10 +0200

    0.

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -8,7 +8,23 @@ function Admin() {
       </a>
       <a href="#">
         <i class=""></i>
+        <span>Client </span>
+      </a>
+      <a href="#">
+        <i class=""></i>
+        <span>Session </span>
+      </a>
+      <a href="#">
+        <i class=""></i>
         <span>Formateur</span>
+      </a>
+      <a href="#">
+        <i class=""></i>
+        <span>Ingenieur Pedagogique</span>
+      </a>
+      <a href="#">
+        <i class=""></i>
+        <span>Support</span>
       </a>
     </div>
   );

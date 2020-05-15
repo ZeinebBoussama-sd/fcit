1cc8eb4395003ba3e7402209e1f6f27bcdb9e9d3
sadzai <sadok.zaibi@gmail.com>
2020-05-15 04:44:31 +0200

    add client to admin

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import Apropos from './Apropos';
 import Formation from '../Admin/Formation/Formation';
+import Client from '../Admin/Client/Client';
 import SideBar from '../Admin/component/SideBar';
 function Admin() {
   return (
@@ -11,7 +12,7 @@ function Admin() {
         <div id='content'>
           <Switch>
             <Route path='/admin/formation' exact component={Formation} />
-            <Route path='/admin/client' exact component={Apropos} />
+            <Route path='/admin/client' exact component={Client} />
             <Route path='/admin/session' exact component={Apropos} />
             <Route path='/admin/formateur' exact component={Apropos} />
             <Route

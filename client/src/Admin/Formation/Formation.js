3dc05f4ec1e905da08dbf6801f71112850b3aa80
sadzai <sadok.zaibi@gmail.com>
2020-05-23 01:25:46 +0200

    fix table

--- client/src/Admin/Formation/Formation.js
+++ client/src/Admin/Formation/Formation.js
@@ -11,39 +11,67 @@ function Formation() {
     <div className='mt-11'>
       <AddFormation />
       <div className='table-responsive'>
-        <table className='mt-2 table table-hover'>
+        <table className='mt-2 table table-hover table-fixed'>
           <thead>
             <tr>
-              <th scope='col'>#</th>
-              <th scope='col'>Intitule</th>
-              <th scope='col'>Durée</th>
-              <th scope='col'>Horaire</th>
-              <th scope='col'>N.Min.Part</th>
-              <th scope='col'>N.Max.Part</th>
-              <th scope='col'>Description</th>
-              <th scope='col'>Catagorie</th>
-              <th scope='col'>Prix</th>
-              <th scope='col'>Prerequis</th>
-              <th scope='col'>Participant</th>
-              <th scope='col'>Theme</th>
+              <th scope='col' className='col-1'>
+                #
+              </th>
+              <th scope='col' className='col-1'>
+                Intitule
+              </th>
+              <th scope='col' className='col-1'>
+                Durée
+              </th>
+              <th scope='col' className='col-1'>
+                Horaire
+              </th>
+              <th scope='col' className='col-1'>
+                N.Min.Part
+              </th>
+              <th scope='col' className='col-1'>
+                N.Max.Part
+              </th>
+              <th scope='col' className='col-1'>
+                Description
+              </th>
+              <th scope='col' className='col-1'>
+                Catagorie
+              </th>
+              <th scope='col' className='col-1'>
+                Prix
+              </th>
+              <th scope='col' className='col-1'>
+                Prerequis
+              </th>
+              <th scope='col' className='col-1'>
+                Participant
+              </th>
+              <th scope='col' className='col-1'>
+                Theme
+              </th>
             </tr>
           </thead>
           <tbody>
             {data.allFormations &&
               data.allFormations.map((formation, idx) => (
                 <tr key={idx}>
-                  <th scope='row'>{idx + 1}</th>
-                  <td>{formation.intitule}</td>
-                  <td>{formation.duree_formation}</td>
-                  <td>{formation.horaire_formation}</td>
-                  <td>{formation.nbre_min_part}</td>
-                  <td>{formation.nbre_max_part}</td>
-                  <td>{formation.description_formation}</td>
-                  <td>{formation.catagorie_formation}</td>
-                  <td>{formation.prix_formation}</td>
-                  <td>{formation.prerequis}</td>
-                  <td>{formation.participant}</td>
-                  <td>{formation.theme && formation.theme.nom_theme}</td>
+                  <th scope='row' className='col-1'>
+                    {idx + 1}
+                  </th>
+                  <td className='col-1'>{formation.intitule}</td>
+                  <td className='col-1'>{formation.duree_formation}</td>
+                  <td className='col-1'>{formation.horaire_formation}</td>
+                  <td className='col-1'>{formation.nbre_min_part}</td>
+                  <td className='col-1'>{formation.nbre_max_part}</td>
+                  <td className='col-1'>{formation.description_formation}</td>
+                  <td className='col-1'>{formation.catagorie_formation}</td>
+                  <td className='col-1'>{formation.prix_formation}</td>
+                  <td className='col-1'>{formation.prerequis}</td>
+                  <td className='col-1'>{formation.participant}</td>
+                  <td className='col-1'>
+                    {formation.theme ? formation.theme.nom_theme : '--'}
+                  </td>
                 </tr>
               ))}
           </tbody>

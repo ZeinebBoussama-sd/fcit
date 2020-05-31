bea4ff4f6d6792a61d08edc61c5ed2817811c729
sadzai <sadok.zaibi@gmail.com>
2020-05-31 23:24:16 +0200

    move out mutatuon

--- client/src/Admin/Formation/AddFormation.js
+++ client/src/Admin/Formation/AddFormation.js
@@ -1,96 +1,64 @@
-import React from "react";
-import gql from "graphql-tag";
-import { useMutation, useQuery } from "@apollo/react-hooks";
-import { Formik } from "formik";
-import { GetTheme } from "../GraphQl/Query";
+import React, { useEffect, useState } from 'react';
+import { useMutation, useQuery } from '@apollo/react-hooks';
+import { Formik } from 'formik';
+import { GetTheme } from '../GraphQl/Query';
+import { ADD_FORMATION } from '../GraphQl/Mutation';
 
 function AddFormation() {
-  const ADD_FORMATION = gql`
-    mutation create_formation(
-      $intitule: String!
-      $duree_formation: Int
-      $horaire_formation: Int
-      $nbre_min_part: Int
-      $nbre_max_part: Int
-      $description_formation: String
-      $catagorie_formation: String
-      $prix_formation: String
-      $participant: String
-      $prerequis: String
-      $ThemeId: Int
-    ) {
-      createFormation(
-        intitule: $intitule
-        duree_formation: $duree_formation
-        horaire_formation: $horaire_formation
-        nbre_min_part: $nbre_min_part
-        nbre_max_part: $nbre_max_part
-        description_formation: $description_formation
-        catagorie_formation: $catagorie_formation
-        prix_formation: $prix_formation
-        participant: $participant
-        prerequis: $prerequis
-        ThemeId: $ThemeId
-      ) {
-        intitule
-        duree_formation
-        horaire_formation
-        nbre_min_part
-        nbre_max_part
-        description_formation
-        catagorie_formation
-        prix_formation
-        participant
-        prerequis
-        ThemeId
-      }
-    }
-  `;
+  useEffect(() => {
+    console.log('submit');
+  }, []);
+
+  const [active, setactive] = useState(false);
+
   const [addFormation] = useMutation(ADD_FORMATION);
   const { loading, error, data } = useQuery(GetTheme);
-  console.log("getTheme", data);
+  console.log('getTheme', data);
+  console.log('active', active);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :{error}(</p>;
   return (
     <div>
       <button
-        type="button"
-        className="btn btn-primary"
-        data-toggle="modal"
-        data-target="#exampleModal"
-        data-whatever="@getbootstrap"
+        type='button'
+        className='btn btn-primary'
+        data-toggle='modal'
+        data-target='#exampleModal'
+        data-whatever='@getbootstrap'
+        onClick={() => setactive(true)}
       >
         Add Formation
       </button>
 
       <div
-        className="modal fade"
-        id="exampleModal"
-        tabIndex="-1"
-        role="dialog"
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
+        className={`modal fade ${active ? 'show' : ''}`}
+        id='exampleModal'
+        tabIndex='-1'
+        role='dialog'
+        aria-modal={`${active ? 'true' : 'false'}`}
+        //aria-labelledby='exampleModalLabel'
+        aria-hidden={`${active ? 'false' : 'true'}`}
       >
-        <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalLabel">
+        <div className='modal-dialog modal-dialog-centered modal-dialog-scrollable'>
+          <div className='modal-content'>
+            <div className='modal-header'>
+              <h5 className='modal-title' id='exampleModalLabel'>
                 Add Formation
               </h5>
               <button
-                type="button"
-                className="close"
-                data-dismiss="modal"
-                aria-label="Close"
+                type='button'
+                className='close'
+                data-dismiss='modal'
+                aria-label='Close'
               >
-                <span aria-hidden="true">&times;</span>
+                <span aria-hidden='true'>&times;</span>
               </button>
             </div>
-            <div className="modal-body">
+            <div className='modal-body'>
               <Formik
                 initialValues={{
-                  intitule: "",
+                  intitule: '',
                   duree_formation: undefined,
                   horaire_formation: undefined,
                   nbre_min_part: undefined,
@@ -104,6 +72,7 @@ function AddFormation() {
                 }}
                 onSubmit={async (values) => {
                   try {
+                    await new Promise((resolve) => setTimeout(resolve, 500));
                     await addFormation({
                       variables: {
                         intitule: values.intitule,
@@ -122,8 +91,9 @@ function AddFormation() {
                       },
                     });
                     alert(JSON.stringify(values, null, 2));
+                    setactive(false);
                   } catch (e) {
-                    console.log("e", e);
+                    console.log('e', e);
                   }
                 }}
                 // validationSchema={Yup.object().shape({
@@ -144,135 +114,135 @@ function AddFormation() {
                   } = props;
                   return (
                     <form onSubmit={handleSubmit}>
-                      <div className="form-group">
-                        <label htmlFor="intitulee" className="col-form-label">
+                      <div className='form-group'>
+                        <label htmlFor='intitulee' className='col-form-label'>
                           Intitule:
                         </label>
                         <input
-                          type="text"
-                          className="form-control"
-                          id="intitule"
+                          type='text'
+                          className='form-control'
+                          id='intitule'
                           onChange={handleChange}
                           value={values.intitule}
                         />
                       </div>
-                      <div className="form-group">
-                        <label htmlFor="Durée" className="col-form-label">
+                      <div className='form-group'>
+                        <label htmlFor='Durée' className='col-form-label'>
                           Durée:
                         </label>
                         <input
-                          type="number"
-                          className="form-control"
-                          id="duree_formation"
+                          type='number'
+                          className='form-control'
+                          id='duree_formation'
                           onChange={handleChange}
-                          value={values.duree_formation || ""}
+                          value={values.duree_formation || ''}
                         />
                       </div>
-                      <div className="form-group">
-                        <label htmlFor="Horaire" className="col-form-label">
+                      <div className='form-group'>
+                        <label htmlFor='Horaire' className='col-form-label'>
                           Horaire:
                         </label>
                         <input
-                          type="number"
-                          className="form-control"
-                          id="horaire_formation"
+                          type='number'
+                          className='form-control'
+                          id='horaire_formation'
                           onChange={handleChange}
-                          value={values.horaire_formation || ""}
+                          value={values.horaire_formation || ''}
                         />
                       </div>
-                      <div className="form-group">
-                        <label htmlFor="N.Min.Part" className="col-form-label">
+                      <div className='form-group'>
+                        <label htmlFor='N.Min.Part' className='col-form-label'>
                           N.Min.Part:
                         </label>
                         <input
-                          type="number"
-                          className="form-control"
-                          id="nbre_min_part"
+                          type='number'
+                          className='form-control'
+                          id='nbre_min_part'
                           onChange={handleChange}
-                          value={values.nbre_min_part || ""}
+                          value={values.nbre_min_part || ''}
                         />
                       </div>
-                      <div className="form-group">
-                        <label htmlFor="N.Max.Part" className="col-form-label">
+                      <div className='form-group'>
+                        <label htmlFor='N.Max.Part' className='col-form-label'>
                           N.Max.Part:
                         </label>
                         <input
-                          type="number"
-                          className="form-control"
-                          id="nbre_max_part"
+                          type='number'
+                          className='form-control'
+                          id='nbre_max_part'
                           onChange={handleChange}
-                          value={values.nbre_max_part || ""}
+                          value={values.nbre_max_part || ''}
                         />
                       </div>
-                      <div className="form-group">
-                        <label htmlFor="Description" className="col-form-label">
+                      <div className='form-group'>
+                        <label htmlFor='Description' className='col-form-label'>
                           Description:
                         </label>
                         <input
-                          type="text"
-                          className="form-control"
-                          id="description_formation"
+                          type='text'
+                          className='form-control'
+                          id='description_formation'
                           onChange={handleChange}
-                          value={values.description_formation || ""}
+                          value={values.description_formation || ''}
                         />
                       </div>
-                      <div className="form-group">
-                        <label htmlFor="Catagorie" className="col-form-label">
+                      <div className='form-group'>
+                        <label htmlFor='Catagorie' className='col-form-label'>
                           Catagorie:
                         </label>
                         <input
-                          type="text"
-                          className="form-control"
-                          id="catagorie_formation"
+                          type='text'
+                          className='form-control'
+                          id='catagorie_formation'
                           onChange={handleChange}
-                          value={values.catagorie_formation || ""}
+                          value={values.catagorie_formation || ''}
                         />
                       </div>
-                      <div className="form-group">
-                        <label htmlFor="Prix" className="col-form-label">
+                      <div className='form-group'>
+                        <label htmlFor='Prix' className='col-form-label'>
                           Prix:
                         </label>
                         <input
-                          type="text"
-                          className="form-control"
-                          id="prix_formation"
+                          type='text'
+                          className='form-control'
+                          id='prix_formation'
                           onChange={handleChange}
-                          value={values.prix_formation || ""}
+                          value={values.prix_formation || ''}
                         />
                       </div>
-                      <div className="form-group">
-                        <label htmlFor="Prerequis" className="col-form-label">
+                      <div className='form-group'>
+                        <label htmlFor='Prerequis' className='col-form-label'>
                           Prerequis:
                         </label>
                         <input
-                          type="text"
-                          className="form-control"
-                          id="prerequis"
+                          type='text'
+                          className='form-control'
+                          id='prerequis'
                           onChange={handleChange}
-                          value={values.prerequis || ""}
+                          value={values.prerequis || ''}
                         />
                       </div>
-                      <div className="form-group">
-                        <label htmlFor="Participant" className="col-form-label">
+                      <div className='form-group'>
+                        <label htmlFor='Participant' className='col-form-label'>
                           Participant:
                         </label>
                         <input
-                          type="text"
-                          className="form-control"
-                          id="participant"
+                          type='text'
+                          className='form-control'
+                          id='participant'
                           onChange={handleChange}
-                          value={values.participant || ""}
+                          value={values.participant || ''}
                         />
                       </div>
-                      <div className="form-group">
-                        <label htmlFor="Theme">Theme</label>
+                      <div className='form-group'>
+                        <label htmlFor='Theme'>Theme</label>
                         <select
-                          className="form-control"
+                          className='form-control'
                           onChange={handleChange}
                           value={values.ThemeId}
-                          id="ThemeId"
+                          id='ThemeId'
                         >
-                          <option value="">---choose theme----</option>
+                          <option value=''>---choose theme----</option>
                           {data.allThemes.map((theme) => {
                             return (
                               <option key={theme.id} value={theme.id}>
@@ -282,18 +252,18 @@ function AddFormation() {
                           })}
                         </select>
                       </div>
-                      <div className="modal-footer">
+                      <div className='modal-footer'>
                         <button
-                          type="button"
-                          className="btn btn-secondary"
-                          data-dismiss="modal"
+                          type='button'
+                          className='btn btn-secondary'
+                          data-dismiss='modal'
                         >
                           Close
                         </button>
                         <button
-                          type="submit"
+                          type='submit'
                           disabled={isSubmitting}
-                          className="btn btn-primary"
+                          className='btn btn-primary'
                         >
                           Add formation
                         </button>
---
+++ client/src/Admin/GraphQl/Mutation.js
@@ -0,0 +1,43 @@
+import { gql } from 'apollo-boost';
+
+export const ADD_FORMATION = gql`
+  mutation create_formation(
+    $intitule: String!
+    $duree_formation: Int
+    $horaire_formation: Int
+    $nbre_min_part: Int
+    $nbre_max_part: Int
+    $description_formation: String
+    $catagorie_formation: String
+    $prix_formation: String
+    $participant: String
+    $prerequis: String
+    $ThemeId: Int
+  ) {
+    createFormation(
+      intitule: $intitule
+      duree_formation: $duree_formation
+      horaire_formation: $horaire_formation
+      nbre_min_part: $nbre_min_part
+      nbre_max_part: $nbre_max_part
+      description_formation: $description_formation
+      catagorie_formation: $catagorie_formation
+      prix_formation: $prix_formation
+      participant: $participant
+      prerequis: $prerequis
+      ThemeId: $ThemeId
+    ) {
+      intitule
+      duree_formation
+      horaire_formation
+      nbre_min_part
+      nbre_max_part
+      description_formation
+      catagorie_formation
+      prix_formation
+      participant
+      prerequis
+      ThemeId
+    }
+  }
+`;
